f80e25163c22898826f9302996ea17529468ad3a
David.P <davidpejoski15@gmail.com>
2025-03-04 20:43:46 +0100

    School work

---
+++ Class-03/Loops this time/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] words = new string[] { "Gamers", "Gaming" ,"Raptor" , "Lockheed"};
+double[] decimalValues = new double[] { 0.35, 32.3, 38.3, 122.3, 1214.12};
+char[] randomCharacters = new char[] { 'A', 'B', 'C', 'D' };
+bool[] randomBoolean = new bool[] { false, true };
+int[][] nestedArray = new int[][]
+{
+    new int[] { 1, 2 }
+};
---
+++ Class-03/Loops/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("===== Creating an Array =====");
+// Array type syntax
+//Array of integers: int[]
+//Array of strings: string[]
+//Array of doubles: double[]
+//Array of characters: char[]
+
+//1. Declaring and initializing an array
+int[] numbers = new int[] { 11, 22, 1, 2, 33, -5, 3 };
+Console.WriteLine(numbers[0]);
+Console.WriteLine(numbers[4]);
+//2. Declaring an array 
+string[] students = new string[3];
+students[0] = "John";
+students[1] = "Doe";
+students[2] = "Jane";
+Console.WriteLine(students[0]);
+
+Console.WriteLine("===== Array Methods =====");
+Console.WriteLine("The numbers array lenght is" + numbers.Length);
+// Sorting through an Array
+Array.Sort(numbers);
+Console.WriteLine(numbers[0]);
+Console.ReadLine();
+
+int indexOfOne = Array.IndexOf(numbers, 1);
+Console.WriteLine($"The index of 1 is {indexOfOne}");
+Console.WriteLine(numbers[indexOfOne]);
+
+Console.WriteLine("===== Adding new items =====");
+//Arrays have a fixed size so we cant directly add elements to them
+
+string[] newStudents = new string[3];
+newStudents[0] = "Bob";
+newStudents[1] = "Joshua";
+newStudents[2] = "Sam";
