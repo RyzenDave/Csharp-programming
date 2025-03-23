670d59b2bd5e126751116a2180cab16f30d6f7f1
David.P <davidpejoski15@gmail.com>
2025-03-23 18:25:30 +0100

    Homework Update

---
+++ Homework/Class-07-Homework/Company/Company/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Company
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string[] Company = Array.Empty<string>();
+        }
+    }
+}
