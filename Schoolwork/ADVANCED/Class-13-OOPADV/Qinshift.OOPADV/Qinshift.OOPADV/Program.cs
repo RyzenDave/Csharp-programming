0c366cd01bbadbc44be23d28b95d71f90736349b
David.P <davidpejoski15@gmail.com>
2025-06-07 15:55:46 +0200

    SchoolWork

---
+++ Schoolwork/ADVANCED/Class-13-OOPADV/Qinshift.OOPADV/Qinshift.OOPADV/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Qinshift.OOPADV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
